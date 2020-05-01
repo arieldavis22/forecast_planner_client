b1e66961774c5e6d95fe63304411248f2ffa5580
laarchambault <laarchambault@gmail.com>
2020-05-01 13:15:54 -0400

    begin front-end signin

--- forecast-planner-client/src/components/Signup.js
+++ forecast-planner-client/src/components/Signup.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
-const Signup = () => {
+class Signup extends React.Component {
+
+    state={
+        name: "",
+        password: "",
+        passwordConfirmation: "",
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
     return (  
-        <h1>Signup</h1>
+        <Fragment>
+            <h1>Signup</h1>
+            <form>
+                <input type="text" name="name" value={this.state.name} onChange={this.handleChange}></input>
+                <input type="password" name="password" value={this.state.password} onChange={this.handleChange}></input>
+                <input type="password" name="passwordConfirmation" value={this.state.passwordConfirmation} onChange={this.handleChange}></input>
+            </form>
+        </Fragment>
     );
+}
 }
 
 export default Signup;
