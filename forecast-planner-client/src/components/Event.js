d26474e5e15ef96fddf50aa3f5d1c08b9ddd9b87
laarchambault <laarchambault@gmail.com>
2020-05-05 16:18:18 -0400

    friend/user cards

--- forecast-planner-client/src/components/Event.js
+++ forecast-planner-client/src/components/Event.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-<<<<<<< HEAD
 import { delEvent } from '../FetchData'
-=======
 import { Card, Button, Image } from 'semantic-ui-react'
->>>>>>> 48092352ae0a1e0299f3e4a50d08e8f9a60ed136
 
 
 
@@ -26,14 +23,11 @@ const Event = props => {
                 {props.event.date}<br/>
                 {props.event.location.replace(",", ", ")}
             </Card.Description>
-            {/* <Card.Header as='h4'>{props.event.location.replace(",", ", ")}</Card.Header> */}
-            
-            
             <Card.Description>
                 {props.event.indoor ? "This is an indoor event" : "This is an outdoor event"}<br/>
                 <strong>Chance of precipitation: {props.event.precipitation_chance}%</strong><br/>
                 {props.event.precipitation_chance > 50 ? "Will probably rain" : "Will probably be sunny"}
-                </Card.Description>
+            </Card.Description>
                 <div >
                     <Button as={NavLink} to={`/edit/${props.event.id}`} floated='left'>Edit</Button>
                     <Button onClick={deleteEvent} color='red' floated='right' size='mini'>X</Button>
